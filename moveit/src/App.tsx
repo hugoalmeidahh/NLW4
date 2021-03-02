8331962fcef5b7f82cf42041ed7b4eaea732c159
Hugo Almeida <hugoalmeidahh@gmail.com>
2021-03-02 09:56:46 -0300

    (HA). create react-app moveit, and removing unnecessary files.

---
+++ moveit/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Opa! My First app react, hummmm, just not.</h1>
+    </div>
+  );
+}
+
+export default App;
